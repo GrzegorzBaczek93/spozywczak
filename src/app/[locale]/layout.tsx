a0b37459dab668c7188ffcacf33bf9ff3ad02aed
Grzegorz Bączek <grzegorz.baczek@silkycoders.com>
2025-10-21 13:41:38 +0200

    Added support for locales

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,30 @@
+import { routing } from "@/src/i18n/routing";
+import "./globals.css";
+import Footer from "@/src/ui/components/footer/footer";
+import Header from "@/src/ui/components/header/header";
+import { hasLocale, NextIntlClientProvider } from "next-intl";
+import { notFound } from "next/navigation";
+
+type Props = {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+};
+
+export default async function RootLayout({ children, params }: Props) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  return (
+    <html lang="pl">
+      <body className="flex min-h-screen flex-col items-center antialiased">
+        <NextIntlClientProvider>
+          <Header />
+          <main className="grow">{children}</main>
+          <Footer />
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
